7d882d9daf9781fbc6215b5bafe0870b66890a56
Noah Wöhler <noah.woehler@gmail.com>
2020-01-10 21:56:30 +0100

    add firebase database driver

---
+++ DataStore.py
@@ -0,0 +1,73 @@
+import firebase_admin
+
+from abc import ABC, abstractmethod
+from firebase_admin import credentials, firestore
+
+
+class DataStore(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def set(self, collection, document, val):
+        pass
+
+    @abstractmethod
+    def update(self, collection, document, val):
+        pass
+
+    @abstractmethod
+    def add(self, collection, val):
+        pass
+
+    @abstractmethod
+    def get(self, collection, document):
+        pass
+
+    @abstractmethod
+    def delete(self, collection, document):
+        pass
+
+
+class FirebaseDataStore(DataStore):
+    def __init__(self, key_file, db_name):
+        super().__init__()
+        cred = credentials.Certificate(key_file)
+
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': f'https://{db_name}.firebaseio.com'
+        })
+
+        self.db = firestore.client()
+
+
+    def set(self, collection, document, val):
+        self._get_doc_ref(collection, document).set(val)
+
+    def update(self, collection, document, val):
+        self._get_doc_ref(collection, document).update(val)
+
+    def add(self, collection, val):
+        self._get_collection(collection).add(val)
+
+    def get(self, collection, document=None):
+        return self._get_collection(collection).stream() if document is None else self._get_doc_ref(collection, document).get()
+
+    def delete(self, collection, document):
+        self._get_doc_ref(collection, document).delete()
+
+    def _get_doc_ref(self, collection, document):
+        return self._get_collection(collection).document(document)
+
+    def _get_collection(self, collection):
+        return self.db.collection(collection)
+
+
+if __name__ == '__main__':
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+
+    firebase_ds = FirebaseDataStore(
+        config['firebase']['key_file'], config['firebase']['db_name'])
+    firebase_ds.add('jobs', {'func': 'somefunc', 'time': 234903284, 'args': ['arg1', 'arg2']})
--- botw-bot.py
+++ botw-bot.py
@@ -3,21 +3,31 @@ import logging
 import cogs
 
 from discord.ext import commands
+from DataStore import FirebaseDataStore
 
 config = configparser.ConfigParser()
 config.read('conf.ini')
 client = commands.Bot(command_prefix=config['discord']['command_prefix'])
 client.config = config
+client.database = FirebaseDataStore(
+    config['firebase']['key_file'], config['firebase']['db_name'])
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='botw-bot.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='botw-bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+
+
+@client.event
+async def on_disconnect():
+    prtint('disconnected')
 
 
 @client.check
