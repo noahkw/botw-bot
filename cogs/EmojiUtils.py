0fcbdde3a685a51ea203e6fdac98104865be5560
Noah <noah@dreckbu.de>
2020-02-03 23:22:42 +0100

    [EmojiUtils] closes #6
    Listen for emoji upload/delete events
    and send new list to specified (config)
    channel.

--- cogs/EmojiUtils.py
+++ cogs/EmojiUtils.py
@@ -1,7 +1,9 @@
 import logging
+import time
 
 import discord
 from discord.ext import commands
+
 from util import chunker
 
 logger = logging.getLogger(__name__)
@@ -13,9 +15,13 @@ def setup(bot):
 
 class EmojiUtils(commands.Cog):
     SPLIT_MSG_AFTER = 15
+    NEW_EMOTE_THRESHOLD = 3600 + 600  # 3600 + number of seconds
+    DELETE_LIMIT = 50
 
     def __init__(self, bot):
         self.bot = bot
+        self.bot.add_listener(self.on_guild_emojis_update, 'on_guild_emojis_update')
+        self.emoji_channel = self.bot.get_channel(int(self.bot.config['emojiutils']['emoji_channel']))
 
     @commands.group(name='emoji')
     @commands.has_permissions(administrator=True)
@@ -31,3 +37,20 @@ class EmojiUtils(commands.Cog):
     @emoji_list.error
     async def emoji_list_error(self, ctx, error):
         await ctx.send(error)
+
+    async def on_guild_emojis_update(self, guild, before, after):
+        # delete old messages containing emoji
+        # need to use Message.delete to be able to delete messages older than 14 days
+        async for message in self.emoji_channel.history(limit=EmojiUtils.DELETE_LIMIT):
+            await message.delete()
+
+        # get emoji that were added in the last 10 minutes
+        recent_emoji = [emoji for emoji in after if (
+                time.time() - discord.utils.snowflake_time(emoji.id).timestamp()) < EmojiUtils.NEW_EMOTE_THRESHOLD]
+
+        emoji_sorted = sorted(after, key=lambda e: e.name)
+        for emoji_chunk in chunker(emoji_sorted, EmojiUtils.SPLIT_MSG_AFTER):
+            await self.emoji_channel.send(''.join(str(e) for e in emoji_chunk))
+
+        if len(recent_emoji) > 1:
+            await self.emoji_channel.send(f"Newly added: {''.join(str(e) for e in recent_emoji)}")
