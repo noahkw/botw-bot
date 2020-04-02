a4e4f915a2bc93a92f7faa5e1595141260a7be16
Noah <noah@dreckbu.de>
2020-04-02 08:10:06 +0200

    [WolframAlpha] add support for image api

--- cogs/WolframAlpha.py
+++ cogs/WolframAlpha.py
@@ -1,7 +1,10 @@
 import logging
 import aiohttp
+import asyncio
 
 from discord.ext import commands
+from discord import File
+from io import BytesIO
 
 logger = logging.getLogger(__name__)
 
@@ -11,26 +14,48 @@ def setup(bot):
 
 
 class WolframAlpha(commands.Cog):
-    API_URL = 'https://api.wolframalpha.com/v1/result'
+    RESULT_API_URL = 'https://api.wolframalpha.com/v1/result'
+    SIMPLE_API_URL = 'https://api.wolframalpha.com/v1/simple'
+
+    MSG_REQUEST_FAILED = 'The request to WolframAlpha\'s API failed.'
+    MSG_EMPTY_RESPONSE = 'Try a different query. WolframAlpha is not cooperating.'
 
     def __init__(self, bot):
         self.bot = bot
         self.app_id = self.bot.config['wolframalpha']['app_id']
+        self.session = aiohttp.ClientSession()
 
-    @commands.command(name='wolframalpha', aliases=['wa'])
-    async def wolfram_alpha(self, ctx, *, query):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                    f'{WolframAlpha.API_URL}?appid={self.app_id}&i={query}'
-            ) as response:
-                if response.status != 200:
-                    await ctx.send('The request to WolframAlpha\'s API failed.'
-                                   )
+    @commands.group(name='wolframalpha',
+                    aliases=['wa'],
+                    invoke_without_command=True)
+    async def wolfram_alpha(self, ctx):
+        await ctx.send_help(self.wolfram_alpha)
+
+    @wolfram_alpha.command(aliases=['s'])
+    async def simple(self, ctx, *, query):
+        async with self.session.get(
+                f'{WolframAlpha.RESULT_API_URL}?appid={self.app_id}&i={query}'
+        ) as response:
+            if response.status != 200:
+                await ctx.send(WolframAlpha.MSG_REQUEST_FAILED)
+            else:
+                content = await response.text()
+                if content is not None:
+                    await ctx.send(content)
                 else:
-                    content = await response.text()
-                    if content is not None:
-                        await ctx.send(content)
-                    else:
-                        await ctx.send(
-                            'Try a different query. WolframAlpha is not cooperating.'
-                        )
+                    await ctx.send(WolframAlpha.MSG_EMPTY_RESPONSE)
+
+    @wolfram_alpha.command(aliases=['i'])
+    async def image(self, ctx, *, query):
+        async with self.session.get(
+                f'{WolframAlpha.SIMPLE_API_URL}?appid={self.app_id}&i={query}'
+        ) as response:
+            if response.status != 200:
+                await ctx.send(WolframAlpha.MSG_REQUEST_FAILED)
+            else:
+                content = await response.read()
+                file = File(BytesIO(content), filename='wolfram.gif')
+                if content is not None:
+                    await ctx.send(file=file)
+                else:
+                    await ctx.send(WolframAlpha.MSG_EMPTY_RESPONSE)
