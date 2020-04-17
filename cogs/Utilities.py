7840bf0c576727a98fc39bd8b8bc82cea5f0969f
Noah <noah@dreckbu.de>
2020-04-18 00:46:39 +0200

    [Utilities] mock command: more checks
    Checks for empty messages after emoji removal
    and whether the message was sent in the same
    channel. Also outputs an error msg if
    the message couldn't be found.

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -70,15 +70,24 @@ class Utilities(commands.Cog):
     @commands.command()
     async def mock(self, ctx, message: Message = None):
         if message:
-            await ctx.send(mock_case(remove_broken_emoji(message.clean_content)))
-        else:
-            valid_msg = None
-            async for msg in ctx.message.channel.history(limit=Utilities.MOCK_HISTORY_LOOKBACK):
-                msg_ctx = await self.bot.get_context(msg)
-                if msg.author != self.bot.user and not msg_ctx.valid:
-                    valid_msg = msg
-                    break
-            if valid_msg is not None:
-                await ctx.send(mock_case(remove_broken_emoji(valid_msg.clean_content)))
+            if message.channel == ctx.channel:
+                await ctx.send(mock_case(remove_broken_emoji(message.clean_content)))
             else:
                 await ctx.message.add_reaction(CROSS_EMOJI)
+        else:
+            valid_msg_content = None
+            async for msg in ctx.message.channel.history(limit=Utilities.MOCK_HISTORY_LOOKBACK):
+                msg_ctx = await self.bot.get_context(msg)
+                content = remove_broken_emoji(msg.clean_content)
+                if msg.author != self.bot.user and not msg_ctx.valid and len(content) > 0:
+                    valid_msg_content = content
+                    break
+            if valid_msg_content is not None:
+                await ctx.send(mock_case(valid_msg_content))
+            else:
+                await ctx.message.add_reaction(CROSS_EMOJI)
+
+    @mock.error
+    async  def mock_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadArgument):
+            await ctx.send("Can't find message with that ID. It's probably ancient.")
