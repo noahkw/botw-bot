b7b7aaeb28258c3f242e1f2d1a2cee280c95284a
Noah <noah@dreckbu.de>
2020-09-10 20:00:17 +0200

    [Gfycat] add format and plot commands

--- cogs/Gfycat.py
+++ cogs/Gfycat.py
@@ -1,11 +1,19 @@
 import asyncio
 import logging
+from io import BytesIO
+
+import discord
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import typing
 from discord.ext import commands
 from discord.ext.menus import MenuPages
 from gfypy import AsyncGfypy
+from gfypy.const import GFYCAT_URL
 
+from cogs import CustomCog, AinitMixin
 from menu import GfyListSource, AsyncGfySource, GfySourceEmpty
 from util import auto_help, DNFParser
 
@@ -40,12 +48,17 @@ class SortByConverter(commands.Converter):
         raise commands.BadArgument(f'Can\'t sort by {argument}. Possible values: `{", ".join(self.OPTIONS)}`')
 
 
-class Gfycat(commands.Cog):
+class Gfycat(CustomCog, AinitMixin):
     def __init__(self, bot):
-        self.bot = bot
+        super().__init__(bot)
         self.gfypy = AsyncGfypy(self.bot.config['gfycat']['client_id'],
                                 self.bot.config['gfycat']['client_secret'],
-                                '../gfycat_creds.json')
+                                'gfycat_creds.json')
+
+        super(AinitMixin).__init__()
+
+    async def _ainit(self):
+        await self.gfypy.authenticate()
 
     def cog_unload(self):
         asyncio.create_task(self.gfypy.close())
@@ -95,6 +108,59 @@ class Gfycat(commands.Cog):
                 gfys = AsyncGenFilter(gfys, lambda gfy: parser.evaluate(gfy.tags))
 
         await self._start_gfy_pagination(ctx, gfys, user_id)
+
+    @gfycat.command(brief='Display the search in a nice-to-copy-paste format', hidden=True)
+    async def format(self, ctx, *, query):
+        parser = DNFParser(query)
+
+        with ctx.typing():
+            gfys = await self.gfypy.get_own_feed()
+            gfys = list(filter(lambda gfy: parser.evaluate(gfy.tags), gfys))
+
+        if len(gfys) == 0:
+            raise commands.BadArgument('Could not find any gfys that meet the given criteria.')
+
+        title = gfys[0].title.split('-')[0].strip()
+
+        links = map(lambda gfy: f'{GFYCAT_URL}/{gfy.gfy_id}', gfys)
+        formatted = f'````{title}`\n' + '\n'.join(links) + '```'
+
+        await ctx.send(formatted)
+
+    @gfycat.command(brief='Display the number of views in a diagram')
+    async def plot(self, ctx, user_id, filter_outliers: typing.Optional[bool] = False,
+                   start: int = None, end: int = None):
+        with ctx.typing():
+            gfys = await self.gfypy.get_user_feed(user_id=user_id, limit=-1)
+
+            data = [(int(gfy.create_date.strftime('%y%m%d')), gfy.views) for gfy in gfys]
+            data = pd.DataFrame(data, columns=['date', 'views'])
+
+            if filter_outliers:
+                data = data[data.views <= data.views.quantile(.85)]
+
+            if start and end:
+                data = data[(data.date >= start) & (data.date <= end)]
+
+            if len(data) == 0:
+                raise commands.BadArgument('Could not find any gfys that meet the given criteria.')
+
+            mean = data.mean()['views']
+            data = data.groupby('date').mean()
+            data.reset_index(inplace=True)
+
+            sns.set(rc={'figure.figsize': (30, 20)})
+            plt.figure()
+            g = sns.barplot(data=data, x='date', y='views')
+            g.locator_params(nbins=9, axis='x')
+
+            buf = BytesIO()
+            plt.savefig(buf, format='png', dpi=200, bbox_inches='tight')
+            buf.seek(0)
+
+            await ctx.send(f'{ctx.author.mention}, here\'s the diagram. Average views per gfy'
+                           f' in the selected timeframe: `{mean:.2f}`.',
+                           file=discord.File(buf, filename='gfycat_views.png'))
 
     async def cog_before_invoke(self, ctx):
         await ctx.trigger_typing()
