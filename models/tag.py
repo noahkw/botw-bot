dce7976739b53e539a3f76f3a5d6629a649a4f7b
Noah <noah@dreckbu.de>
2020-11-02 01:34:11 +0100

    Tag model: change DateTime type, make columns non-nullable

--- models/tag.py
+++ models/tag.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime
+
 import discord
 from sqlalchemy import (
     Column,
@@ -7,14 +7,13 @@ from sqlalchemy import (
     BigInteger,
     Integer,
     Boolean,
-    DateTime,
     update,
     delete,
 )
 from sqlalchemy.ext.hybrid import hybrid_property
 
 from util import safe_mention
-from .base import Base
+from .base import Base, PendulumDateTime
 
 IMAGE_URL_REGEX = r"https?:\/\/.*\.(jpe?g|png|gif)"
 
@@ -24,13 +23,13 @@ class Tag(Base):
     EDITABLE = ["trigger", "reaction", "in_msg"]
 
     tag_id = Column(Integer, primary_key=True)
-    trigger = Column(String)
-    reaction = Column(String)
-    in_msg = Column(Boolean)
-    _creator = Column(BigInteger)
-    _guild = Column(BigInteger)
+    trigger = Column(String, nullable=False)
+    reaction = Column(String, nullable=False)
+    in_msg = Column(Boolean, default=False)
+    _creator = Column(BigInteger, nullable=False)
+    _guild = Column(BigInteger, nullable=False)
     use_count = Column(Integer, default=0)
-    date = Column(DateTime, default=datetime.utcnow)
+    date = Column(PendulumDateTime, default=PendulumDateTime.now())
 
     @hybrid_property
     def creator(self):
@@ -54,13 +53,13 @@ class Tag(Base):
 
     def info_embed(self):
         embed = (
-            discord.Embed(title=f"Tag `{self.tag_id}`")
+            discord.Embed(title=f"Tag `{self.tag_id}`", timestamp=self.date)
             .add_field(name="Trigger", value=self.trigger)
             .add_field(name="Reaction", value=self.reaction)
             .add_field(name="Creator", value=safe_mention(self.creator))
             .add_field(name="Triggers in message", value=str(self.in_msg))
             .add_field(name="Use Count", value=str(self.use_count))
-            .set_footer(text=f"Created on {self.date}")
+            .set_footer(text="Created")
         )
 
         if re.search(IMAGE_URL_REGEX, self.reaction):
